51254852d88630fbfaa8f16bea7eae684ae0cc03
gabrielavqofashvili <gabrielavqofashvili18@gmail.com>
2024-02-13 16:02:17 +0400

    first commit

---
+++ day 1/data.py
@@ -0,0 +1,6 @@
+name="gabriel "
+surname="avqofashvili "
+age=9
+
+print (name + surname + str(age)+" years old")
+
---
+++ day 3/homework/lesson3.py
@@ -0,0 +1,8 @@
+print("natalia azaladze")
+#this is my mother
+print("kakha avqofashvili")
+#this is my father
+print("luka avqofashvili")
+#this is my brother
+print("gabriel avqofashvili")
+#this is me
